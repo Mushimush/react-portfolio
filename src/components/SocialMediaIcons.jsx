02081029230e07763dda5e5e0fe52879157bf1b9
Stanley <stanleytanyr@gmail.com>
2023-08-14 22:57:48 +0800

    img

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -2,10 +2,10 @@ const SocialMediaIcons = () => {
     return (
         <div className="flex justify-center md:justify-start my-10 gap-7">
             <a href="/" className="hover:opacity-50 transition duration-500" target="_blank" rel="noreferrer">
-                <img className="h-8 w-8" src="src/assets/linkedin.png" alt="linkedin-link" />
+                <img className="h-8 w-8" src="/assets/linkedin.png" alt="linkedin-link" />
             </a>
             <a href="/" className="hover:opacity-50 transition duration-500" target="_blank" rel="noreferrer">
-                <img className="h-8 w-8" src="src/assets/github-mark-white.png" alt="github-link" />
+                <img className="h-8 w-8" src="/assets/github-mark-white.png" alt="github-link" />
             </a>
         </div>
     )
